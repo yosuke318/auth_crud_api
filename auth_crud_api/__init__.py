6fd791973cb69b0dbea337a0f2f378836e486b60
Yosuke Maruyama <youman.318@gmail.com>
2025-02-16 14:10:18 +0900

    refactor: improve  reusability by introducing abstraction

--- auth_crud_api/__init__.py
+++ auth_crud_api/__init__.py
@@ -1,5 +1,5 @@
 from authentication.admin_auth_api import AdminAuthApi
 from authentication.auth_api import AuthApi
-from auth_crud_api.rdb_crud.rdb_crud_handler import RDBCrudHandler
+from rdb_crud.rdb_crud_handler import RDBCrudHandler
 
 __all__ = ['AuthApi', 'AdminAuthApi', 'RDBCrudHandler']
